27f6a0654ec2a691ce48c467c65e712a44f0ee0f
YunchinLin <bess861231@gmail.com>
2017-03-10 13:54:02 +0800

    The progress of abstraction 2

---
+++ src/oo/abstraction/EMR.java
@@ -0,0 +1,16 @@
+package oo.abstraction;
+
+public class EMR {
+	int ChartNumber;
+	String IdentityCard;
+	String Name;
+	char Gender;
+	int Birthday;
+	public EMR(int ChartNumber, String IdentityCard, String Name, char Gender, int Birthday){
+		this.ChartNumber = ChartNumber;
+		this.IdentityCard = IdentityCard;
+		this.Name = Name;
+		this.Gender = Gender;
+		this.Birthday = Birthday;
+	}
+}
