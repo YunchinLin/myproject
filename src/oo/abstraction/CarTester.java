eab7c981e815a3187424dbb55def5a3958734982
user <user@es302n19>
2017-03-14 15:57:26 +0800

    新增建構子(Constructor)

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,9 +3,9 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota", "Altis", 1800, "Sedan");
-		//
-		//
+		Car c1 = new Car("Toyota", "Altis", 1800);
+		Car c2 = new Car("Honda", "Accord", 2000);
+		Car c3 = new Car("Toyota", "Wish", 2000);
 	}
 
 }
