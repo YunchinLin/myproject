41691d8780ff18bb1c9cd551909fd84fce9ef99a
yunchin_lin <bess861231@gmail.com>
2017-03-25 20:59:33 +0800

    Array in Java, primitive vs reference 2

--- src/oo/abstraction/Student.java
+++ src/oo/abstraction/Student.java
@@ -1,21 +1,20 @@
 package oo.abstraction;
 
 public class Student {
+	int seatnumber;
 	String school;
 	String major;
 	int idnumber;
 	String name;
 	String gender;
 	int grade;
-	int credit;
 	int location;
-	public Student(String school, String major, int idnumber, String name, String gender, int grade, int credit){
+	public Student(String school, String major, int idnumber, String name, String gender, int grade){
 		this.school = school;
 		this.major = major;
 		this.idnumber = idnumber;
 		this.name = name;
 		this.gender = gender;
 		this.grade = grade;
-		this.credit= credit;
 	}
 }
--- src/oo/abstraction/StudentTester.java
+++ src/oo/abstraction/StudentTester.java
@@ -3,7 +3,18 @@ package oo.abstraction;
 public class StudentTester {
 
 	public static void main(String[] args) {
-		Student s1 = new Student("輔仁大學", "醫資學程", 405570173, "林芸妗", "女", 1, 23);
+		Student s1 = new Student("輔仁大學", "醫資學程", 405570173, "林芸妗", "女", 1);
+		Student s2 = new Student("輔仁大學", "醫資學程", 405570202, "蔡德蓉", "女", 1);
+		Student s3 = new Student("輔仁大學", "醫資學程", 405570214, "張芳菽", "女", 1);
+		
+		Student[] ss = new Student[3];
+		ss[0] = new Student("輔仁大學", "醫資學程", 405570173, "林芸妗", "女", 1);
+		ss[1] = new Student("輔仁大學", "醫資學程", 405570202, "蔡德蓉", "女", 1);
+		ss[2] = new Student("輔仁大學", "醫資學程", 405570214, "張芳菽", "女", 1);
+		ss[0].seatnumber = 17;
+		ss[1].seatnumber = 20;
+		ss[2].seatnumber = 21;
+		
 	}
 
 }
