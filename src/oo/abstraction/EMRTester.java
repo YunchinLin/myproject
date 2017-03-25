30b2768ae77e8d0ccaea0b29445027a00b281f4c
yunchin_lin <bess861231@gmail.com>
2017-03-25 20:49:33 +0800

    Array in Java, primitive vs reference 1

--- src/oo/abstraction/EMRTester.java
+++ src/oo/abstraction/EMRTester.java
@@ -3,11 +3,8 @@ package oo.abstraction;
 public class EMRTester {
 
 	public static void main(String[] args) {
-		EMR e1 = new EMR(00000001, "A123456789", "代刚1", 'M', 20010101);
-		EMR e2 = new EMR(00000002, "B123456789", "代刚2", 'F', 20010202);
-		EMR e3 = new EMR(00000003, "C123456789", "代刚3", 'M', 20010303);
-		EMR e4 = new EMR(00000004, "D123456789", "代刚4", 'F', 20010404);
-		EMR e5 = new EMR(00000005, "E123456789", "代刚5", 'M', 20010505);
+		// TODO Auto-generated method stub
+
 	}
 
 }
