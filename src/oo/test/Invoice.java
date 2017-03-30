f4515320c7e4948ce1579c8d294596803353187c
yunchin_lin <bess861231@gmail.com>
2017-03-30 18:04:43 +0800

    Practice for ArrayList & Invoice

--- src/oo/test/Invoice.java
+++ src/oo/test/Invoice.java
@@ -6,21 +6,29 @@ import java.util.Scanner;
 public class Invoice {
 
 	public static void main(String[] args) {
-		String s = "331";
-		//----------0123456789
-		String n = "1234567331";
-		System.out.println(n.length());
-		String sub = n.substring(n.length()-3);
-		System.out.println(sub);
+//		String s = "331";
+//		//----------0123456789
+//		String n = "1234567331";
+//		System.out.println(n.length());
+//		String sub = n.substring(n.length()-3);
+//		System.out.println(sub);
 		
 		ArrayList<String> a = new ArrayList();
 		a.add("331");
 		a.add("821");
 		a.add("886");
 		a.add("554");
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("請輸入發票號碼:");
-		String num = scanner.nextLine();
+		while(true){
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("請輸入發票號碼:");
+			String num = scanner.nextLine();
+			String sub = num.substring(num.length()-3);
+			if (a.get(0).equals(sub) || a.get(1).equals(sub) || a.get(2).equals(sub) || a.get(3).equals(sub)){
+				System.out.println("您中獎惹!");
+			}else{
+				System.out.println("摃龜摟~");
+			}
+		}
 	}
 
 }
