4ae048006389fb20a7753d79e9c974792ef62862
YunchinLin <bess861231@gmail.com>
2017-04-11 16:32:33 +0800

    inheritance-0

--- src/oo/stati/Student.java
+++ src/oo/stati/Student.java
@@ -5,6 +5,9 @@ public class Student {
 	int math;
 	int chinese;
 	static int pass = 60;
+	public Student(){
+		
+	}
 //	public Student(int english, int math, int chinese){
 //		this.english = english;
 //		this.math = math;
