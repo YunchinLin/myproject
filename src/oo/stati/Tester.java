fcc399f7c7d1250c7b22122e3ad16a068982a7db
yunchin_lin <bess861231@gmail.com>
2017-05-08 21:29:36 +0800

    use List process Student and GraduateStudent objects (update)

--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -24,8 +24,8 @@ public class Tester {
 		list.add(new Student(58, 98, 66));
 		list.add(new Student(67, 56, 77));
 		list.add(new Student(88, 99, 85));
-		list.add(new Student(68, 85, 89));
-		list.add(new Student(78, 89, 60));
+		list.add(new GraduateStudent(68, 85, 89));
+		list.add(new GraduateStudent(78, 89, 60));
 		for (int i=0; i<list.size(); i++){
 			Student stu = list.get(i);
 			stu.print();
