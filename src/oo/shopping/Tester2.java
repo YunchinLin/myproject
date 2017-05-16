2a7505c2f063303f310e129e46fabbc5d3227d3d
YunchinLin <bess861231@gmail.com>
2017-05-16 16:26:07 +0800

    for each & 轉型

--- src/oo/shopping/Tester2.java
+++ src/oo/shopping/Tester2.java
@@ -6,21 +6,32 @@ public class Tester2 {
 
 	public static void main(String[] args) {
 		System.out.println("客戶級別"+"\t"+"消費金額"+"\t"+"折扣後金額"+"\t"+"還元金");
-		ArrayList<Customer> list = new ArrayList();
+		ArrayList<Customer> list = new ArrayList<>();
 		list.add(new Customer(6000));
 		list.add(new SilverCustomer(8000));
 		list.add(new SilverCustomer(10000));
 		list.add(new GoldenCustomer(30000));
 		list.add(new Customer(2000));
 		list.add(new GoldenCustomer(10000));
-		for(int i=0; i<list.size(); i++){
-			Customer cus = list.get(i); 
+//		for(int i=0; i<list.size(); i++){
+//			Customer cus = list.get(i); 
+		for (Customer cus : list){
 			if ((cus instanceof SilverCustomer) && 
 				!(cus instanceof GoldenCustomer)){
 				System.out.print("*");
+				SilverCustomer silver = (SilverCustomer)cus;
 			}
 			cus.print2();
 		}
+		
+		/*String[] array = {"aa","bb","cc"};
+		for (String s : array){
+			System.out.println(s);
+		}
+		for (int i=0; i<array.length; i++){
+			String s = array[i];
+			System.out.println(s);
+		}*/
 	}
 
 }
